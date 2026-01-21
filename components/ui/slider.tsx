5888b9b86b594fd70698895efdc59e77addde23b
raulvan <vanraul@outlook.com>
2026-01-21 10:22:16 +0800

    fix slider value typing for cloudflare build

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -4,13 +4,20 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface SliderProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "onChange" | "value" | "defaultValue"
+  > {
   value?: number[];
+  defaultValue?: number[];
   onValueChange?: (value: number[]) => void;
 }
 
 const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
-  ({ className, value, onValueChange, min = 0, max = 100, ...props }, ref) => {
+  (
+    { className, value, defaultValue, onValueChange, min = 0, max = 100, ...props },
+    ref
+  ) => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       onValueChange?.([parseInt(e.target.value, 10)]);
     };
@@ -21,7 +28,8 @@ const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
         ref={ref}
         min={min}
         max={max}
-        value={value?.[0] ?? 0}
+        value={value?.[0]}
+        defaultValue={defaultValue?.[0]}
         onChange={handleChange}
         className={cn(
           "w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer accent-rose-500",
